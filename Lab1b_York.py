635e301678fe2c7181100332998e77570e24ba68
yorkcs <yorkcs@outlook.com>
2016-09-23 22:07:06 -0400

    Update Lab1b_York.py

--- Lab1b_York.py
+++ Lab1b_York.py
@@ -1,11 +1,11 @@
 def createAPicture():
 
-  #Gathering info from user
+  # Gathering info from user
   fname = requestString("What is your first name?")
   myColor = pickAColor()
   side = requestInteger("How many pixels will be on the side of the picture?")
 
-  #Making a picture with gathered info
+  # Making a picture with gathered info
   userPic = makeEmptyPicture(side, side, myColor)
   printNow("Hello, " + fname + ". Your colored picture is displayed on the screen!")
   show(userPic)
