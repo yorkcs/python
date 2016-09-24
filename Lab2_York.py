436a004b45570658ab50192b093df4601e3cdec1
yorkcs <yorkcs@outlook.com>
2016-09-23 22:06:54 -0400

    Update Lab2_York.py

--- Lab2_York.py
+++ Lab2_York.py
@@ -8,12 +8,12 @@ def main():
   show(pic)
   showInformation("The picture\'s width is " + str(width) + ".\nThe picture\'s height is " + str(height) + ".")
 
-  #Repainting picture to user selected color
+  # Repainting picture to user selected color
   userColor = pickAColor()
   setAllPixelsToAColor(pic, userColor)
   repaint(pic)
 
-  #Adding shapes to picture
+  # Adding shapes to picture
   addRectFilled(pic, 0, 0, 60, 60, white)
   addRectFilled(pic, width-60, height-60, 60, 60, white)
   addOvalFilled(pic, (width-60)/2, (height-60)/2, 60, 60, yellow)
